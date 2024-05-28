bfda98acfb27aa82bab069ab35c405fd6e3d533e
AgustinCastino <aguscastino01@gmail.com>
2024-05-28 17:01:14 -0300

    Controllers

---
+++ src/controladores/GuiaController.java
@@ -0,0 +1,27 @@
+package controladores;
+
+public class GuiaController {
+
+    private Guia guia;
+
+    public GuiaController(Guia guia) {
+        this.guia = guia;
+    }
+
+    public void registrarGuia(GuiaDTO usuarioDTO, ModoAuth modoRegistro){
+        // Implementacion
+        return;
+    }
+
+    public void loginGuia(GuiaDTO usuarioDTO, ModoAuth modoLogin){
+        // Implementacion
+        return;
+    }
+
+    public List<GuiaDTO> buscarGuias(GuiaDTO guiaDTO){
+        // Implementacion
+        return null;
+    }
+
+
+}
