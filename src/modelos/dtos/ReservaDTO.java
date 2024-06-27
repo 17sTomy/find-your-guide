8c8916da1ab126b4dfe492669f86b8f4da7b4253
AgustinCastino <aguscastino01@gmail.com>
2024-06-26 21:36:09 -0300

    Se crean DTOs Factura y Reserva para pasar en ViajeDTO

---
+++ src/modelos/dtos/ReservaDTO.java
@@ -0,0 +1,11 @@
+package modelos.dtos;
+
+import modelos.clases.Reserva;
+
+public class ReservaDTO {
+    private int id;
+
+    public ReservaDTO(Reserva reserva) {
+        this.id = id;
+    }
+}
